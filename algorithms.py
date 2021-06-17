fc598b6c421d6e6b947e15ee4b1a3911c8f99564
Neel Ismail <neelismail01@gmail.com>
2021-06-17 14:21:22 -0400

    adjusting pivot for quicksort implementation

--- algorithms.py
+++ algorithms.py
@@ -1,11 +1,11 @@
 def partition(arr, start, end):
-    pivot = arr[start]
+    pivot = arr[(start + end) // 2]
     i = start
     j = end
     while i < j:
         while arr[i] < pivot:
             i += 1
-        while arr[j] > pivot:
+        while j >= i and arr[j] > pivot:
             j -= 1
         if i < j:
             arr[i], arr[j] = arr[j], arr[i]
@@ -14,16 +14,16 @@ def partition(arr, start, end):
 def quick_sort(arr, start, end):
     if start < end:
         pivot = partition(arr, start, end)
-        quick_sort(arr, start, pivot - 1)
+        quick_sort(arr, start, pivot)
         quick_sort(arr, pivot + 1, end)
 
 
 def printArray(arr, n):
     for i in range(n):
-        print(arr[i], end=" ")
+        print(arr[i], end=' ')
     print()
 
-arr = [10, 5, 3, 6, 7, 2, 9]
-n = len(arr)
-quick_sort(arr, 0, n - 1)
-printArray(arr, n)
+if __name__ == '__main__':
+    arr = [4, 10, 5, 3, 6, 7, 2, 9]
+    quick_sort(arr, 0, len(arr) - 1)
+    printArray(arr, len(arr))
