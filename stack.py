b6d7575a612f0e6d91ef7b7026198b2f4a30eb31
Neel Ismail <neelismail01@gmail.com>
2021-07-02 01:52:28 -0400

    implementing stack ADT

---
+++ stack.py
@@ -0,0 +1,38 @@
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def insert_first(self, new_element):
+        new_element.next = self.head
+        self.head = new_element
+
+    def delete_first(self):
+        deletedElement = None
+        if self.head:
+            deletedElement = self.head
+            self.head = self.head.next
+        return deletedElement
+
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        return self.ll.delete_first()
