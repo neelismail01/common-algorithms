fee2c1ff9ced03b6000fdbcac3df13124285a273
Neel Ismail <neelismail01@gmail.com>
2021-09-23 12:57:28 -0400

    implementing breadth first search

---
+++ breadth-first-search.py
@@ -0,0 +1,23 @@
+### format of graph provided to function
+### graph = {
+###   'A' = ['B', 'C'],
+###   'B' = ['A', 'E', 'G']
+###   'C' = ...
+### }
+
+
+def breadthFirstSearch(graph, node):
+    visited = []
+    queue = []
+
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        s = queue.pop(0)
+        print(s)
+
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
