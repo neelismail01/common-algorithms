5275d7c58613e144c33a300845fd2af6c7119659
Neel Ismail <neelismail01@gmail.com>
2021-09-23 12:21:41 -0400

    implementing quick sort algorithm

--- algorithms/quick-sort.py
+++ algorithms/quick-sort.py
@@ -1,33 +1,24 @@
-def partition(arr, left, right):
-    pivot = arr[(left + right) // 2]
-    while left <= right:
-        while arr[left] < pivot:
-            left += 1
-        while right >= left and arr[right] > pivot:
-            right -= 1
+def quickSort(array):
+	quickSortHelper(array, 0, len(array) - 1)
+	return array
 
-        if left <= right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
+def quickSortHelper(array, startIdx, endIdx):
+	if startIdx >= endIdx:
+		return
+	pivotIdx = startIdx
+	leftIdx = startIdx + 1
+	rightIdx = endIdx
+	while rightIdx >= leftIdx:
+		if array[leftIdx] > array[pivotIdx] and array[rightIdx] < array[pivotIdx]:
+			swap(leftIdx, rightIdx, array)
+		if array[leftIdx] <= array[pivotIdx]:
+			leftIdx += 1
+		if array[rightIdx] >= array[pivotIdx]:
+			rightIdx -= 1
+	swap(pivotIdx, rightIdx, array)
+	quickSortHelper(array, startIdx, rightIdx - 1)
+	quickSortHelper(array, rightIdx + 1, endIdx)
+		
+def swap(i, j, array):
+	array[i], array[j] = array[j], array[i]
 
-    return left
-
-def quick_sort(arr, left, right):
-    index = partition(arr, left, right)
-    if left < index - 1:
-        quick_sort(arr, left, index - 1)
-
-    if index < right:
-        quick_sort(arr, index, right)
-
-
-def printArray(arr, n):
-    for i in range(n):
-        print(arr[i], end=' ')
-    print()
-
-if __name__ == '__main__':
-    arr = [4, 10, 5, 3, 6, 7, 2, 9]
-    quick_sort(arr, 0, len(arr) - 1)
-    printArray(arr, len(arr))
